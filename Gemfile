7afa7170ed8d3e74d5474b561ede60941d9ee412
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-23 21:54:35 +0000

    Identified authentication problem: data was put in arrays not hashes.

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'rack-test'
 gem 'sinatra-twitter-bootstrap', :require => 'sinatra/twitter-bootstrap'
 gem 'capybara'
 gem 'require_all'
-gem "bcrypt"
+gem 'bcrypt'
 
 group :development do
        gem 'shotgun'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,9 +36,9 @@ class UsersController < ApplicationController
   post '/login' do
 
     @user = User.find_by(username: params[:username])
-    binding.pry
     if @user && @user.authenticate(params[:password])
       session[:user_id] = params[:id]
+      @hikes = Hike.all
       erb :hikes
     else
       redirect '/login'
